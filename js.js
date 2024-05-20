9664dae4a3e1161e434dbe9a8d694290a1990416
Antonin GUILBAUD <antonin.guilbaud@etu.univ-grenoble-alpes.fr>
2024-05-20 14:04:54 +0200

    test menu

--- js.js
+++ js.js
@@ -80,3 +80,11 @@ document.addEventListener("mousemove", e => {
   });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menuButton');
+    const menuList = document.getElementById('menuList');
+
+    menuButton.addEventListener('click', function() {
+        menuList.classList.toggle('show');
+    });
+});
